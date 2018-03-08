088989da16855a4028239aedb177a65c477ea923
Berenice Agustina Larsen Pereyra <berelarsenp@gmail.com>
2018-03-08 04:21:53 +0000

    Advanced collections practice working!

--- step_1.py
+++ step_1.py
@@ -1,2 +1,8 @@
 def transform_products_to_list(products_string):
-    pass
+    split_products = products_string.split('\n')
+    products_list = []
+    for products_line in split_products:
+        if products_line:
+            product = products_line.split(',')
+            products_list.append(product)
+    return products_list
--- step_4.py
+++ step_4.py
@@ -1,5 +1,13 @@
 from step_1 import transform_products_to_list
 
-
 def calculate_total_per_invoices(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+    
+    products = {}
+    for product in products_list:
+        customer_id = product[-2]
+        invoice_id = product[0]
+        products.setdefault(customer_id, {})
+        products[customer_id].setdefault(invoice_id, 0)
+        products[customer_id][invoice_id] += round((float(product[-3]) * float(product[-5])), 2)
+    return products
